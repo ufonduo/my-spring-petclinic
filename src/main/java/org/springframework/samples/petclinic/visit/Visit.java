b9f1805e054d3112cc32828a65e34b30d2e9d177
ufonduo <ufonduo@gmail.com>
2024-04-15 17:40:47 +0100

    Upgraded to JDK 21 and Spring Boot 3.2.4, using OpenRewrite.
    1. Upgraded Maven Wrapper via "mvn -N wrapper:wrapper -Dmaven=3.9.6" executed on the project root folder.
    
    2. Successfully upgraded to JDK 21 using OpenRewrite "org.openrewrite.java.migrate.UpgradeToJava21" recipe.
    
    3. Upgraded to Spring Boot 3.2.4 using OpenRewrite "org.openrewrite.java.spring.boot3.UpgradeSpringBoot_3_2" recipe. Note that for the "jakarta.xml.bind-api" and "jaxb-runtime" artefact, their version had to be explicitly set (e.g. <version>4.0.2</version> & <version>4.0.5</version> respectively); for this recipe to work.
    
    NOTE: This was done using JDK 17 configured on the IDE project settings, and Maven version 3.9.6. Once completed, had to switch to JDK 21 on the IDE project settings.

--- src/main/java/org/springframework/samples/petclinic/visit/Visit.java
+++ src/main/java/org/springframework/samples/petclinic/visit/Visit.java
@@ -17,12 +17,12 @@ package org.springframework.samples.petclinic.visit;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotEmpty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotEmpty;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.samples.petclinic.model.BaseEntity;
